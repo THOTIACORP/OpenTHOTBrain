b3286a365cab04d7eec54d3a6c3aab6839bc2c6d
THOTIACORP <90805434+THOTIACORP@users.noreply.github.com>
2024-03-01 23:27:46 -0400

    ✨ feat: Multitent config

--- src/back-end/run.py
+++ src/back-end/run.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 app.register_blueprint(test_connection_db)
 app.register_blueprint(connection_db)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Roda o aplicativo em modo de depuração na porta 5000
+    app.run(debug=True, host='0.0.0.0', port=5000)
