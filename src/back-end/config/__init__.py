b83e9f1791e60956bb5fb302f16cb0c0a758d28c
THOTIACORP <90805434+THOTIACORP@users.noreply.github.com>
2024-03-01 23:24:23 -0400

    ✨ feat: Multitent config

---
+++ src/back-end/config/__init__.py
@@ -0,0 +1,2 @@
+from .test.test_connection_db import test_connection_db
+from .routes.connection import connection_db
--- src/back-end/run.py
+++ src/back-end/run.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from .config.test.test_connection_db import test_connection_db
+from config import test_connection_db, connection_db
 
 app = Flask(__name__)
 
 # Registrar as rotas
 app.register_blueprint(test_connection_db)
+app.register_blueprint(connection_db)
 
 if __name__ == "__main__":
     app.run(debug=True)
